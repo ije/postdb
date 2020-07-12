85d31981e86bd087aba8120261e4296911e2e7c6
Je <git@iamje.com>
2020-07-12 14:54:54 +0800

    refactor: rewrite server api

--- server.go
+++ server.go
@@ -4,17 +4,13 @@ import (
 	"crypto/tls"
 )
 
-type ServerConfig struct {
+type Server struct {
 	DB        *NSDB
 	Port      uint16
 	Secret    string
 	TLSConfig *tls.Config
 }
 
-type server struct {
-	db *NSDB
-}
-
-func Serve(config ServerConfig) error {
+func (s *Server) Serve() error {
 	return nil
 }
